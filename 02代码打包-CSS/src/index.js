e05247e5881ff3eb91669baba85d2222ae6652d9
严俊东 <1172482914@qq.com>
2017-03-10 20:14:38 +0800

    代码打包-CSS

---
+++ 02代码打包-CSS/src/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by 严俊东 on 2017/3/10.
+ */
+import "./css/main.css"
+import "./css/test.css"
+console.log("hello");
---
+++ 02代码打包-CSS/webpack.config.js
@@ -0,0 +1,28 @@
+/**
+ * Created by 严俊东 on 2017/3/10.
+ */
+
+var path = require('path');
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: 'css-loader'
+                })
+            }
+        ]
+    }, plugins: [
+        new ExtractTextPlugin('style.css'),
+    ]
+};
